6cd897c0b938903fa85ce81bff8633e77c2c239c
Chanina Szendro <cszendro@yahoo.com>
2013-12-12 12:49:15 -0500

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,13 @@
 class SessionsController < ApplicationController
-  def new
+	def new
   end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       sign_in user
-      redirect_to user
+			flash[:success] = "Welcome back!"
+      redirect_back_or user
     else
       flash.now[:error] = 'Invalid email/password combination'
       render 'new'
@@ -15,6 +16,7 @@ class SessionsController < ApplicationController
 
   def destroy
 	  sign_out
+		flash[:notice] = "You are logged out."
     redirect_to root_url
   end
 end
