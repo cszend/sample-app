2e628a65b406cd4ac3c4df19cc56f257461198be
Chanina Szendro <cszendro@yahoo.com>
2013-12-16 18:19:56 -0500

    Add user posts

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,12 +4,41 @@ describe "Static pages" do
 
   subject { page }
 
+	it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact'))
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title('Sign up'))
+  end
+	
   describe "Home page" do
     before { visit root_path }
 
     it { should have_content('Meet and Exceed') }
     it { should have_title(full_title('')) }
     it { should_not have_title('| Home') }
+
+		describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+        sign_in user
+        visit root_path
+      end
+
+      it "should render the user's feed" do
+        user.feed.each do |item|
+          expect(page).to have_selector("li##{item.id}", text: item.content)
+        end
+      end
+    end
   end
 
   describe "Help page" do
@@ -32,18 +61,4 @@ describe "Static pages" do
     it { should have_selector('h1', text: 'Contact') }
     it { should have_title(full_title('Contact')) }
   end
-	
-	it "should have the right links on the layout" do
-    visit root_path
-    click_link "About"
-    expect(page).to have_title(full_title('About Us'))
-    click_link "Help"
-    expect(page).to have_title(full_title('Help'))
-    click_link "Contact"
-    expect(page).to have_title(full_title('Contact'))
-    click_link "Home"
-    click_link "Sign up now!"
-    expect(page).to have_title(full_title('Sign up'))
-  end
-
 end
